7472be69faf08a9f609b5bf76113b73f1f0f6d31
uhhfeef <afeefkhan99@gmail.com>
2025-01-08 16:08:19 +0400

    created main containing siderbar main

--- app/components/nav-main.tsx
+++ app/components/nav-main.tsx
@@ -17,4 +17,24 @@ import {
       DropdownMenuTrigger,
     } from "./ui/dropdown-menu"  
   
-  
+export function NavMain({items}: {items: {title: string, url: string, icon: React.ComponentType<{ className?: string }>}[]}) {
+    return (
+        <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+        <SidebarGroupLabel>Application</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    )
+  }
